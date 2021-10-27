ca2885f690e652106c5c1ff8d214fc965bcc34c5
Cwbertrand <91189853+Cwbertrand@users.noreply.github.com>
2021-10-27 16:31:51 +0200

    init

---
+++ config/connect.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'db.php';
+
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if ($mysqli->connect_error) {
+    die('connection failed : '.$mysqli->connect_error);
+}
+
+function query(string $sql, bool $unique = false): array
+{
+    global $mysqli;
+
+    $results = [];
+
+    if ($result = $mysqli->query($sql)) {
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                array_push($results, $row);
+            }
+        }
+        $result->close();
+    }
+
+    return $unique === true && !empty($results) ? $results[0] : $results;
+}
---
+++ sql/size.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 27, 2021 at 12:53 PM
+-- Server version: 8.0.26
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mugs_party`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `size`
+--
+
+DROP TABLE IF EXISTS `size`;
+CREATE TABLE IF NOT EXISTS `size` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `s` varchar(40) NOT NULL,
+  `m` varchar(40) NOT NULL,
+  `xl` varchar(40) NOT NULL,
+  `xxl` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
